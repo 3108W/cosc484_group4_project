8f4f2b4815dc49dc74e8a8d39286ad86eb100cdf
Ezekiel Halley <eze.halley@gmail.com>
2022-04-08 03:51:23 -0400

    init

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const element =<h1>Hello, world</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(element)
